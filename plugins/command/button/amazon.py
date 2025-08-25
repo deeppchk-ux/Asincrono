d138ca44990098207a9f4f5db9e1c66fd2cbccf2
deeppchk-ux <deeppchk@gmail.com>
2025-08-25 13:55:06 -0600

    Actualizar amazon.py

--- plugins/command/button/amazon.py
+++ plugins/command/button/amazon.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from crateres.textos_after import cmds_texto
 
 @Client.on_callback_query(filters.regex("^amz$"))
 async def gates(client, callback_query):  # Cambiado a async def, parámetro a callback_query
